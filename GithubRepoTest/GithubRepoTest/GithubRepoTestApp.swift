ee565a9fac20048d933af553a2f08aaa5a14f8d4
Eugenio di Lorenzo <gege92.edl@gmail.com>
2025-03-13 12:10:45 +0100

    Sample Project creation

---
+++ GithubRepoTest/GithubRepoTest/GithubRepoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepoTestApp.swift
+//  GithubRepoTest
+//
+//  Created by Eugenio di Lorenzo on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubRepoTest/GithubRepoTestTests/GithubRepoTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepoTestTests.swift
+//  GithubRepoTestTests
+//
+//  Created by Eugenio di Lorenzo on 13/03/25.
+//
+
+import Testing
+@testable import GithubRepoTest
+
+struct GithubRepoTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
